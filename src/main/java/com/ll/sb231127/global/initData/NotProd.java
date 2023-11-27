448e9b648acc97d4eb615c0acc2e3870e38ad65b
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-27 15:52:48 +0900

    feat: Add not prod class

---
+++ src/main/java/com/ll/sb231127/global/initData/NotProd.java
@@ -0,0 +1,22 @@
+package com.ll.sb231127.global.initData;
+
+import com.ll.sb231127.domain.member.entity.Member;
+import com.ll.sb231127.domain.member.service.MemberService;
+import com.ll.sb231127.global.rsData.RsData;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("!prod")
+@Configuration
+public class NotProd {
+    @Bean
+    public ApplicationRunner initNotProdData(MemberService memberService) {
+        return args ->{
+            RsData<Member> joinRs = memberService.join("user1", "1234");
+            System.out.println(joinRs.getMsg());
+        };
+    }
+
+}
